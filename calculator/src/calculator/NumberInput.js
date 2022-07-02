e65ea9f365a2bb7eab3fac8688bf0d43bb25fcf5
pete-bucek <107347218+pete-bucek@users.noreply.github.com>
2022-07-02 10:14:57 +0200

    Add files via upload

---
+++ calculator/src/calculator/NumberInput.js
@@ -0,0 +1,39 @@
+/*
+ *  _____ _______         _                      _
+ * |_   _|__   __|       | |                    | |
+ *   | |    | |_ __   ___| |___      _____  _ __| | __  ___ ____
+ *   | |    | | '_ \ / _ \ __\ \ /\ / / _ \| '__| |/ / / __|_  /
+ *  _| |_   | | | | |  __/ |_ \ V  V / (_) | |  |   < | (__ / /
+ * |_____|  |_|_| |_|\___|\__| \_/\_/ \___/|_|  |_|\_(_)___/___|
+ *                   ___
+ *                  |  _|___ ___ ___
+ *                  |  _|  _| -_| -_|  LICENCE
+ *                  |_| |_| |___|___|
+ *
+ * IT ZPRAVODAJSTVÍ  <>  PROGRAMOVÁNÍ  <>  HW A SW  <>  KOMUNITA
+ *
+ * Tento zdrojový kód pochází z IT sociální sítě WWW.ITNETWORK.CZ
+ *
+ * Můžete ho upravovat a používat jak chcete, musíte však zmínit
+ * odkaz na http://www.itnetwork.cz
+ */
+
+import React from 'react';
+
+let NumberInput = (props) => {
+	const { name, label } = props;
+	return (
+		<label htmlFor={name}>
+			{label}
+			<input
+				onChange={props.OnChange}
+				id={name}
+				type="number"
+				name={name}
+				required
+			/>
+		</label>
+	);
+};
+
+export default NumberInput;
